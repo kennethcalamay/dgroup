4bf901d6fecbc57d825e938f6aae7264d8fac80c
Kenneth Calamay <dev+kenneth@proudcloud.net>
2012-08-08 17:22:12 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dgroup::Application.config.secret_token = '883d1b984b82fd6675151c7dc23f94db6d7d586ae4e918821fa8c6f52919b4c3d7b3cfbd0084fb0188aea28f3eaedff66790ae026bbc8398b039bee869a5a4b4'
